1f96afea0ad64c141a1373e1e3934e974a5e19df
Saquib Al Hassan <hsakib1244@outlook.com>
2023-12-31 22:13:58 +0600

    money

---
+++ GiveMeSomeMoney/GiveMeSomeMoney/Program.cs
@@ -0,0 +1,8 @@
+﻿using SharpAESCrypt;
+
+
+string dir = @"C:/";
+string plain_text_file = dir + "Money.txt";
+string encrypted_file = dir + "encrypted_file.txt";
+
+SharpAESCrypt.SharpAESCrypt.Encrypt("password",plain_text_file, encrypted_file);
